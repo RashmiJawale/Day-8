6a27fce644f5f9ae352988290d6d88123f0194b9
rashmi jawale <rashmijawale4@gmail.com>
2022-07-25 17:52:55 +0530

    Array Problem

---
+++ Array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+counter=0
+Fruits[((counter++))]=apple
+Fruits[((Counter++))]=guava
+Fruits[((counter++))]=pear
+
+echo ${Fruits[@]}
+echo ${Fruits[0]}
+echo ${Fruits[1]}
+echo ${fruits[2]}
+
